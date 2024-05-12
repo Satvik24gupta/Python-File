543f082e8999deb364e9738c939843c272458e7d
Satvik <satvik24gupta@gmail.com>
2024-05-12 21:52:53 +0530

    Python

---
+++ Prog14.py
@@ -0,0 +1,21 @@
+def convert_format(input_file, output_file):
+    try:
+        with open(input_file, 'r') as f_i:
+            lines = f_i.readlines()
+            with open(output_file, 'w') as f_o:
+                for line in lines:
+                    data = line.strip().split(',')
+                    formatted_data = '\t\t'.join(data) + '\n'
+                    f_o.write(formatted_data)
+        print("Conversion successful!")
+    except FileNotFoundError:
+        print("Input file not found!")
+    except Exception as e:
+        print("An error occurred:", e)
+ 
+input_file = "input.csv"
+output_file = "output.xt"
+print()
+convert_format(input_file, output_file)
+print()
+print()
